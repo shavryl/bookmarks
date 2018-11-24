576ae6c25f8df7883473e1e8769d4820727e0fa3
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-11-24 21:08:57 +0200

    user list view and absolute_url

--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
@@ -24,8 +25,16 @@ class Contact(models.Model):
                                    db_index=True)
 
     class Meta:
-        ordering = '-created'
+        ordering = ('-created',)
 
     def __str__(self):
         return '{} follows {}'.format(self.user_form,
                                       self.user_to)
+
+
+# Add following field to User dynamically
+User.add_to_class('following',
+                  models.ManyToManyField('self',
+                                         through=Contact,
+                                         related_name='followers',
+                                         symmetrical=False))
--- account/views.py
+++ account/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 from .forms import LoginForm, UserRegistrationForm,\
                     UserEditForm, ProfileEditForm
 from .models import Profile
@@ -77,3 +79,19 @@ def user_login(request):
 def dashboard(request):
     return render(request, 'account/dashboard.html',
                   {'selection': 'dashboard'})
+
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(request, 'account/user/list.html',
+                  {'section': 'people', 'users': users})
+
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User,
+                             username=username,
+                             is_active=True)
+    return render(request, 'account/user/detail.html',
+                  {'section': 'people', 'user': user})
